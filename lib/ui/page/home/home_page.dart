c4133d8703f6560f52122cbc4beb5d1d05f5bf3f
Hoang T.Phuong <phuonggm204@gmail.com>
2025-11-20 15:04:04 +0700

    edit HomePageUI

--- lib/ui/page/home/home_page.dart
+++ lib/ui/page/home/home_page.dart
@@ -15,46 +15,55 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
           Align(
             alignment: Alignment.topRight,
             child: Container(
               height: 222,
-              width: MediaQuery.widthOf(context),
+              width: double.infinity,
               decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage(AppImages.headerImg), fit: BoxFit.cover),
+                image: DecorationImage(
+                  image: AssetImage(AppImages.headerImg),
+                  fit: BoxFit.cover,
+                ),
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("October 20, 2022", style: AppTextStyle.titleSmall,),
-                  SizedBox(height: 30,),
-                  Text("My Todo List", style: AppTextStyle.titleApp,)
+                  Text("October 20, 2022", style: AppTextStyle.titleSmall),
+                  SizedBox(height: 20),
+                  Text("My Todo List", style: AppTextStyle.titleApp),
                 ],
               ),
             ),
           ),
-          Align(
-            alignment: Alignment.topRight,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 110, right: 16, left: 16),
-              child: ListView(
+
+          Expanded(
+            child: Transform.translate(
+              offset: Offset(0, -50),
+              child: Container(
+                margin: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  color: Colors.white,
+                ),
+
+                child: ListView(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  children: [for (int i = 0; i < 14; i++) TodoItem()],
+                ),
               ),
             ),
-
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Padding(
 
-              padding: const EdgeInsets.all(16.0),
-              child: ButtonPurple(
-                onTap: (){},
-                textButton: "Add New Task",
-              ),
-            ),
-          )
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ButtonPurple(onTap: () {}, textButton: "Add New Task"),
+          ),
         ],
       ),
     );
