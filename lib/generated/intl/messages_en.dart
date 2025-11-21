4b74bfe50a0181962b5c75d77e4d9e4323ef143a
Hoang T.Phuong <phuonggm204@gmail.com>
2025-11-21 10:22:52 +0700

    feat: l10n

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,38 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "button_add_new_task": MessageLookupByLibrary.simpleMessage("Add New Task"),
+    "hint_notes": MessageLookupByLibrary.simpleMessage("Notes"),
+    "hint_task_title": MessageLookupByLibrary.simpleMessage("Task Title"),
+    "hint_time": MessageLookupByLibrary.simpleMessage("Time"),
+    "label_category": MessageLookupByLibrary.simpleMessage("Category"),
+    "label_completed": MessageLookupByLibrary.simpleMessage("Completed"),
+    "label_date": MessageLookupByLibrary.simpleMessage("Date"),
+    "label_notes": MessageLookupByLibrary.simpleMessage("Notes"),
+    "label_task_title": MessageLookupByLibrary.simpleMessage("Task Title"),
+    "label_time": MessageLookupByLibrary.simpleMessage("Time"),
+    "title_add_new_task": MessageLookupByLibrary.simpleMessage("Add New Task"),
+    "title_app": MessageLookupByLibrary.simpleMessage("My Todo List"),
+  };
+}
---
+++ lib/generated/l10n.dart
@@ -0,0 +1,165 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(
+      _current != null,
+      'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.',
+    );
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(
+      instance != null,
+      'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?',
+    );
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Add New Task`
+  String get button_add_new_task {
+    return Intl.message(
+      'Add New Task',
+      name: 'button_add_new_task',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Add New Task`
+  String get title_add_new_task {
+    return Intl.message(
+      'Add New Task',
+      name: 'title_add_new_task',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Task Title`
+  String get label_task_title {
+    return Intl.message(
+      'Task Title',
+      name: 'label_task_title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Task Title`
+  String get hint_task_title {
+    return Intl.message(
+      'Task Title',
+      name: 'hint_task_title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Category`
+  String get label_category {
+    return Intl.message('Category', name: 'label_category', desc: '', args: []);
+  }
+
+  /// `Date`
+  String get label_date {
+    return Intl.message('Date', name: 'label_date', desc: '', args: []);
+  }
+
+  /// `Time`
+  String get label_time {
+    return Intl.message('Time', name: 'label_time', desc: '', args: []);
+  }
+
+  /// `Time`
+  String get hint_time {
+    return Intl.message('Time', name: 'hint_time', desc: '', args: []);
+  }
+
+  /// `Notes`
+  String get label_notes {
+    return Intl.message('Notes', name: 'label_notes', desc: '', args: []);
+  }
+
+  /// `Notes`
+  String get hint_notes {
+    return Intl.message('Notes', name: 'hint_notes', desc: '', args: []);
+  }
+
+  /// `My Todo List`
+  String get title_app {
+    return Intl.message('My Todo List', name: 'title_app', desc: '', args: []);
+  }
+
+  /// `Completed`
+  String get label_completed {
+    return Intl.message(
+      'Completed',
+      name: 'label_completed',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[Locale.fromSubtags(languageCode: 'en')];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
