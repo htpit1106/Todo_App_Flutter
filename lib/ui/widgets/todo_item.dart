c34d3dd5dd19e0eba5db06930531b8b39d3aadae
Hoang T.Phuong <phuonggm204@gmail.com>
2025-11-24 11:55:47 +0700

    update todo_item

--- lib/ui/widgets/todo_item.dart
+++ lib/ui/widgets/todo_item.dart
@@ -3,7 +3,16 @@ import 'package:todo_app/common/app_icons.dart';
 import 'package:todo_app/common/app_text_style.dart';
 
 class TodoItem extends StatelessWidget {
-  const TodoItem({super.key});
+  const TodoItem({
+    super.key,
+    required this.isCompleted,
+    this.titleTask,
+    this.time,
+  });
+
+  final bool isCompleted;
+  final String? titleTask;
+  final String? time;
 
   @override
   Widget build(BuildContext context) {
@@ -15,26 +24,58 @@ class TodoItem extends StatelessWidget {
         color: Colors.white,
         border: Border(bottom: BorderSide(color: Colors.grey, width: 1.0)),
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: isCompleted
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
 
-        children: [
-          Image.asset(AppIcons.icCategoryTask),
-          SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('Run 5k', style: AppTextStyle.bodyMedium),
-                Text("4:00 pm", style: AppTextStyle.bodySmall),
+                Image.asset(AppIcons.icCategoryTask),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        titleTask ?? "no title",
+                        style: TextStyle(
+                          decoration: TextDecoration.lineThrough,
+                          color: Color(0xFF1B1B1D),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      Text(time ?? "no time", style: AppTextStyle.bodySmall),
+                    ],
+                  ),
+                ),
+                Image.asset(AppIcons.icCheckedFalse),
+              ],
+            )
+          : Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Image.asset(AppIcons.icCategoryTask),
+                SizedBox(width: 16),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        titleTask ?? "no title",
+                        style: AppTextStyle.bodyMedium,
+                      ),
+                      Text(time ?? "no time", style: AppTextStyle.bodySmall),
+                    ],
+                  ),
+                ),
+                Image.asset(AppIcons.icCheckedTrue),
               ],
             ),
-          ),
-          Image.asset(AppIcons.icCheckedTrue),
-        ],
-      ),
     );
   }
 }
